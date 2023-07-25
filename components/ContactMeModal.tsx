afcd3766feb254437ee541e5894a9f7fd573b160
Damiano Schirinzi <damianoschirinzi.dev@gmail.com>
2023-07-25 21:11:15 +0100

    Spelling error

--- components/ContactMeModal.tsx
+++ components/ContactMeModal.tsx
@@ -69,7 +69,7 @@ const ContactModal = ({ isOpen, onClose }: ContactModalProps) => {
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" className="" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" fill="white"></path> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" fill="white"></path> </svg>
 
-            <h1 className="pt-8 pb-4 text-xl"> Your message has been sent <strong className="text-custom_highlight">succesfully!</strong>  ✅ </h1>
+            <h1 className="pt-8 pb-4 text-xl"> Your message has been sent <strong className="text-custom_highlight">successfully!</strong>  ✅ </h1>
             <p className="text-sm leading-6 "> I appreciate your message and will reply as soon as possible.</p>
           </motion.div>
         ) : (
